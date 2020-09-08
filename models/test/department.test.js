d30d5d4aeb2f1ac4c7e23fdfcee6cb1f83301157
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-09-08 17:20:05 +0200

    Add tests for department and employee model

---
+++ models/test/department.test.js
@@ -0,0 +1,53 @@
+const Department = require('../department.model');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Department', () => {
+    after(() => {
+        mongoose.models = {};
+    });
+
+    it('should throw an error if no "name" arg', () => {
+        const singleDepartment = new Department({});
+
+        singleDepartment.validate(err => {
+            expect(err.errors.name).to.exist;
+        });
+    });
+
+    it('should throw an error if "name" is not a string', () => {
+        const cases = [{}, []];
+
+        for(let name of cases) {
+          const singleDepartment = new Department({ name });
+      
+          singleDepartment.validate(err => {
+            expect(err.errors.name).to.exist;
+          });
+        }
+    });
+    it('should throw an error if "name" length is to short/long', () => {
+        const cases = ['a', 'ab', 'abc', 'abcd', 'Department of highest intelligence'];
+
+        for(let name of cases) {
+          const singleDepartment = new Department({ name });
+      
+          singleDepartment.validate(err => {
+            expect(err.errors.name).to.exist;
+          });
+        }
+    });
+    it('should not throw any error if "name" is valid', () => {
+        const cases = ['abcde', 'Marketing', 'Testing', 'Management'];
+
+        for(let name of cases) {
+          const singleDepartment = new Department({ name });
+      
+          singleDepartment.validate(err => {
+            expect(err).not.exist;
+          });
+        }
+    });
+});
+
+
---
+++ models/test/employee.test.js
@@ -0,0 +1,85 @@
+const Employee = require('../employee.model');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+const e = require('express');
+
+describe('Employee', () => {
+    after(() => {
+        mongoose.models = {};
+    });
+
+    const employee = {
+        firstName: 'John',
+        lastName: 'Doe',
+        department: 'Marketing',
+    };
+
+    it('should throw an error if no arg given', () => {
+        const singleEmployee = new Employee({});
+
+        singleEmployee.validate(err => {
+            expect(err.errors).to.exist;
+        });
+    });
+    it('should throw an error if no "firstName" is given', () => {
+        const singleEmployee = new Employee({ lastName: employee.lastName, department: employee.department});
+
+        singleEmployee.validate(err => {
+            expect(err.errors.firstName).to.exist;
+        });
+    });
+    it('should throw an error if no "lastName" is given', () => {
+        const singleEmployee = new Employee({ firstName: employee.firstName, department: employee.department});
+
+        singleEmployee.validate(err => {
+            expect(err.errors.lastName).to.exist;
+        });
+    });
+    it('should throw an error if no "department" is given', () => {
+        const singleEmployee = new Employee({ firstName: employee.firstName, lastName: employee.lastName});
+
+        singleEmployee.validate(err => {
+            expect(err.errors.department).to.exist;
+        });
+    });
+    it('should throw an error if "firstName" is not a string', () => {
+        const cases = [{}, []];
+
+        for(let firstName of cases) {
+            const singleEmployee = new Employee({ firstName: firstName, lastName: employee.lastName, department: employee.department });
+
+            singleEmployee.validate(err => {
+                expect(err.errors.firstName).to.exist;
+            });
+        }
+    });
+    it('should throw an error if "lastName" is not a string', () => {
+        const cases = [{}, []];
+
+        for(let lastName of cases) {
+            const singleEmployee = new Employee({ firstName: employee.firstName, lastName: lastName, department: employee.department });
+
+            singleEmployee.validate(err => {
+                expect(err.errors.lastName).to.exist;
+            });
+        }
+    });
+    it('should throw an error if "department" is not a string', () => {
+        const cases = [{}, []];
+
+        for(let department of cases) {
+            const singleEmployee = new Employee({ firstName: employee.firstName, lastName: employee.lastName, department: department });
+
+            singleEmployee.validate(err => {
+                expect(err.errors.department).to.exist;
+            });
+        }
+    });
+    it('should not throw any error if args are valid', () => {
+        const singleEmployee = new Employee({ firstName: employee.firstName, lastName: employee.lastName, department: employee.department });
+
+        singleEmployee.validate(err => {
+            expect(err).not.exist;
+        });
+    });
+});
